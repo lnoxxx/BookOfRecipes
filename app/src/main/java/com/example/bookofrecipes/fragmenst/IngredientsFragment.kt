3ebfb9f0c25f6d8c96e029adaf75afd670f6f24e
lnoxxx <zezezer20@gmail.com>
2023-10-16 00:42:19 +0700

    Update IngredientsFragment.kt

--- app/src/main/java/com/example/bookofrecipes/fragmenst/IngredientsFragment.kt
+++ app/src/main/java/com/example/bookofrecipes/fragmenst/IngredientsFragment.kt
@@ -1,19 +1,29 @@
 package com.example.bookofrecipes.fragmenst
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
+import android.widget.EditText
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import com.example.bookofrecipes.R
+import com.example.bookofrecipes.dataBase.AppDatabase
 import com.example.bookofrecipes.dataBase.Ingredient
 import com.example.bookofrecipes.databinding.FragmentIngredientsBinding
 import com.example.bookofrecipes.rcAdapters.IngredientRcAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class IngredientsFragment : Fragment() {
 
     private lateinit var bindingIngredients: FragmentIngredientsBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,17 +31,76 @@ class IngredientsFragment : Fragment() {
     ): View {
         bindingIngredients = FragmentIngredientsBinding.inflate(inflater)
 
-        val ingredient1 = Ingredient(name = "Соль")
-        val ingredient2 = Ingredient(name = "Сахар")
-        val ingredient3 = Ingredient(name = "Лист салата")
 
-        val ingredientList = arrayOf(ingredient1,ingredient2,ingredient3)
+        val database = Room.databaseBuilder(
+            requireContext(),
+            AppDatabase::class.java,
+            "my_database"
+        ).build()
 
         bindingIngredients.rcIngredients.layoutManager = LinearLayoutManager(context)
-        val adapter = IngredientRcAdapter(ingredientList)
-        bindingIngredients.rcIngredients.adapter = adapter
+        bindingIngredients.rcIngredients.clipToPadding = false
+        bindingIngredients.rcIngredients.setPadding(0, 0, 0, 200)
+
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val ingredientList = database.ingredientDao().getAllIngredients()
+            withContext(Dispatchers.Main) {
+                val adapter = IngredientRcAdapter(ingredientList)
+                bindingIngredients.rcIngredients.adapter = adapter
+            }
+        }
+
+        bindingIngredients.button2.setOnClickListener{
+            showInputDialog()
+        }
+
+        bindingIngredients.rcIngredients.animate().apply {
+            duration = 300
+            alpha(1f)
+        }
 
         return bindingIngredients.root
+    }
+
+
+    private fun showInputDialog() {
+
+        val builder = AlertDialog.Builder(context,R.style.CustomAlertDialogStyle)
+
+        val view = layoutInflater.inflate(R.layout.dialoge_input, null)
+        val editText = view.findViewById<EditText>(R.id.editTextText3)
+        builder.setView(view)
+
+        builder.setNegativeButton("Отмена", DialogInterface.OnClickListener { dialog, _ ->
+            dialog.dismiss()
+        })
+
+        builder.setPositiveButton("Добавить", DialogInterface.OnClickListener { dialog, _ ->
+            val inputText = editText.text.toString()
+
+            val database = Room.databaseBuilder(
+                requireContext(),
+                AppDatabase::class.java,
+                "my_database"
+            ).build()
+
+            val addedIngredient = Ingredient(name = inputText)
+
+            CoroutineScope(Dispatchers.IO).launch {
+                database.ingredientDao().insert(addedIngredient)
+                val ingredientList = database.ingredientDao().getAllIngredients()
+                withContext(Dispatchers.Main) {
+                    val adapter = IngredientRcAdapter(ingredientList)
+                    bindingIngredients.rcIngredients.adapter = adapter
+                }
+            }
+
+            dialog.dismiss()
+        })
+
+        val dialog = builder.create()
+        dialog.show()
     }
 
 
