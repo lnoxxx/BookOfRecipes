202912c8e9673a013abe9f9d4a9e2db4fd1b7724
lnoxxx <zezezer20@gmail.com>
2023-10-15 14:50:32 +0700

    Create AppDatabaseEntity.kt

---
+++ app/src/main/java/com/example/bookofrecipes/dataBase/AppDatabaseEntity.kt
@@ -0,0 +1,27 @@
+package com.example.bookofrecipes.dataBase
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ingredients")
+data class Ingredient(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String
+)
+
+@Entity(tableName = "recipes")
+data class Recipe(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val type: String,
+    val recipeText: String,
+    val isFavorite: Boolean
+)
+
+@Entity(tableName = "recipesAndIngredients")
+data class RecipesAndIngredients(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val recipeId: Long,
+    val ingredientId: Long,
+    val howMany: String,
+)
