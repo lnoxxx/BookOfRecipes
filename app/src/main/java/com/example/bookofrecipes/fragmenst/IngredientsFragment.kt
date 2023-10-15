8cbf61c56d2b22e6da454eece2acaca8b87e9a92
lnoxxx <zezezer20@gmail.com>
2023-10-15 16:33:26 +0700

    Update IngredientsFragment.kt

--- app/src/main/java/com/example/bookofrecipes/fragmenst/IngredientsFragment.kt
+++ app/src/main/java/com/example/bookofrecipes/fragmenst/IngredientsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bookofrecipes.dataBase.Ingredient
 import com.example.bookofrecipes.databinding.FragmentIngredientsBinding
+import com.example.bookofrecipes.rcAdapters.IngredientRcAdapter
 
 class IngredientsFragment : Fragment() {
 
@@ -16,6 +20,19 @@ class IngredientsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         bindingIngredients = FragmentIngredientsBinding.inflate(inflater)
+
+        val ingredient1 = Ingredient(name = "Соль")
+        val ingredient2 = Ingredient(name = "Сахар")
+        val ingredient3 = Ingredient(name = "Лист салата")
+
+        val ingredientList = arrayOf(ingredient1,ingredient2,ingredient3)
+
+        bindingIngredients.rcIngredients.layoutManager = LinearLayoutManager(context)
+        val adapter = IngredientRcAdapter(ingredientList)
+        bindingIngredients.rcIngredients.adapter = adapter
+
         return bindingIngredients.root
     }
+
+
 }
