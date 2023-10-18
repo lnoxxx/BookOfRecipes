1f703847897562c23105b0ba8c3ee7b18bee2f2f
lnoxxx <zezezer20@gmail.com>
2023-10-18 22:51:09 +0700

    Update AddFragment.kt

--- app/src/main/java/com/example/bookofrecipes/fragmenst/AddFragment.kt
+++ app/src/main/java/com/example/bookofrecipes/fragmenst/AddFragment.kt
@@ -5,18 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.example.bookofrecipes.dataBase.AppDatabase
+import com.example.bookofrecipes.dataBase.Category
 import com.example.bookofrecipes.databinding.FragmentAddBinding
+import com.example.bookofrecipes.rcAdapters.CategoryRcAdapter
+import com.example.bookofrecipes.rcAdapters.ChoseCategoryRecyclerViewAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class AddFragment : Fragment() {
+class AddFragment : Fragment(), ChoseCategoryRecyclerViewAdapter.Listener {
 
     private lateinit var bindingAdd: FragmentAddBinding
+
+    private var chosenId: Int = 1
+
+    lateinit var adapter:ChoseCategoryRecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         bindingAdd = FragmentAddBinding.inflate(inflater)
+
+        val database = Room.databaseBuilder(
+            requireContext(),
+            AppDatabase::class.java,
+            "my_database"
+        ).build()
+
+        bindingAdd.choseCategoryRV.layoutManager =
+            LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val categoryList = database.CategoryDao().getAllCategories()
+            withContext(Dispatchers.Main) {
+                adapter = ChoseCategoryRecyclerViewAdapter(this@AddFragment,categoryList)
+                bindingAdd.choseCategoryRV.adapter = adapter
+            }
+        }
+
+
         return bindingAdd.root
     }
+
+    override fun onClick(category: Category) {
+        chosenId = category.id.toInt()
+        adapter.changeChose(category.id.toInt())
+    }
+
 
 }
