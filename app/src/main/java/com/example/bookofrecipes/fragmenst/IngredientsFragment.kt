4e0e78ac7893ff6c0e4ee363faa9c3feb4651de7
lnoxxx <zezezer20@gmail.com>
2023-10-18 22:51:13 +0700

    Update IngredientsFragment.kt

--- app/src/main/java/com/example/bookofrecipes/fragmenst/IngredientsFragment.kt
+++ app/src/main/java/com/example/bookofrecipes/fragmenst/IngredientsFragment.kt
@@ -3,12 +3,16 @@ package com.example.bookofrecipes.fragmenst
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import com.example.bookofrecipes.R
 import com.example.bookofrecipes.dataBase.AppDatabase
@@ -38,10 +42,13 @@ class IngredientsFragment : Fragment() {
             "my_database"
         ).build()
 
-        bindingIngredients.rcIngredients.layoutManager = LinearLayoutManager(context)
+        bindingIngredients.rcIngredients.layoutManager =
+            LinearLayoutManager(context)
         bindingIngredients.rcIngredients.clipToPadding = false
         bindingIngredients.rcIngredients.setPadding(0, 0, 0, 200)
 
+        val swapHelper = getSwapMng()
+        swapHelper.attachToRecyclerView(bindingIngredients.rcIngredients)
 
         CoroutineScope(Dispatchers.IO).launch {
             val ingredientList = database.ingredientDao().getAllIngredients()
@@ -53,11 +60,6 @@ class IngredientsFragment : Fragment() {
 
         bindingIngredients.button2.setOnClickListener{
             showInputDialog()
-        }
-
-        bindingIngredients.rcIngredients.animate().apply {
-            duration = 300
-            alpha(1f)
         }
 
         return bindingIngredients.root
@@ -72,9 +74,6 @@ class IngredientsFragment : Fragment() {
         val editText = view.findViewById<EditText>(R.id.editTextText3)
         builder.setView(view)
 
-        builder.setNegativeButton("Отмена", DialogInterface.OnClickListener { dialog, _ ->
-            dialog.dismiss()
-        })
 
         builder.setPositiveButton("Добавить", DialogInterface.OnClickListener { dialog, _ ->
             val inputText = editText.text.toString()
@@ -103,5 +102,77 @@ class IngredientsFragment : Fragment() {
         dialog.show()
     }
 
+
+    private fun getSwapMng(): ItemTouchHelper{
+        return ItemTouchHelper(object : ItemTouchHelper.SimpleCallback
+            (0,ItemTouchHelper.LEFT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+
+                val database = Room.databaseBuilder(
+                    requireContext(),
+                    AppDatabase::class.java,
+                    "my_database"
+                ).build()
+
+                val builder = AlertDialog.Builder(context,R.style.CustomAlertDialogStyle)
+
+                val view = layoutInflater.inflate(R.layout.dialog_delete, null)
+                builder.setView(view)
+
+                builder.setPositiveButton("Удалить", DialogInterface.OnClickListener { dialog, _ ->
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val ingredientList = database.ingredientDao().getAllIngredients()
+                        database.ingredientDao().
+                        deleteIngredient(ingredientList[viewHolder.adapterPosition])
+                        val ingredientListNew = database.ingredientDao().getAllIngredients()
+                        withContext(Dispatchers.Main) {
+                            val adapter = IngredientRcAdapter(ingredientListNew)
+                            bindingIngredients.rcIngredients.adapter = adapter
+                        }
+                    }
+                    bindingIngredients.rcIngredients.alpha = 0f
+                    bindingIngredients.rcIngredients.animate().apply {
+                        duration = 500
+                        alpha(1f)
+                    }
+                    dialog.dismiss()
+                })
+
+
+
+                val dialog = builder.create()
+
+                dialog.setOnDismissListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val ingredientList = database.ingredientDao().getAllIngredients()
+                        withContext(Dispatchers.Main) {
+                            val adapter = IngredientRcAdapter(ingredientList)
+                            bindingIngredients.rcIngredients.adapter = adapter
+                        }
+                    }
+                    bindingIngredients.rcIngredients.alpha = 0f
+                    bindingIngredients.rcIngredients.animate().apply {
+                        duration = 500
+                        alpha(1f)
+                    }
+                }
+
+                dialog.show()
+
+                CoroutineScope(Dispatchers.IO).launch {
+
+                }
+            }
+        })
+    }
 
 }
